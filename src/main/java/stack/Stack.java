7c150e3538c0770fe06f4774798c4b87c4360513
vsaxena12 <36344027+vsaxena12@users.noreply.github.com>
2025-09-17 07:10:33 -0500

    Starting collections

--- src/main/java/stack/Stack.java
+++ src/main/java/stack/Stack.java
@@ -56,10 +56,8 @@ public class Stack <V> {
         }
     }
 
-
-
     public static void main(String[] args) {
-        Stack stack = new Stack(5);
+        Stack<Integer> stack = new Stack<>(5);
         stack.push(1);
         stack.push(2);
         stack.push(3);
